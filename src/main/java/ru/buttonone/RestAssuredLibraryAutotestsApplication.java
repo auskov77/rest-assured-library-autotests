524143197dbab86858adf6edf30249cb9463e38c
auskov77 <auskov77@yandex.ru>
2022-10-07 22:23:42 +0300

    API tests for library autotests

---
+++ src/main/java/ru/buttonone/RestAssuredLibraryAutotestsApplication.java
@@ -0,0 +1,13 @@
+package ru.buttonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredLibraryAutotestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAssuredLibraryAutotestsApplication.class, args);
+	}
+
+}
